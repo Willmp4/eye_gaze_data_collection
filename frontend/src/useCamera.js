4308f22be51e3a8c3457ab3a26a495bc9b05256e
willmp4 <w.goudie@outlook.com>
2023-12-28 12:20:42 +0000

    Create useCamera

---
+++ frontend/src/useCamera.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from "react";
+
+const useCamera = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      });
+    }
+  }, []);
+
+  const captureImage = (onCapture = (blob) => {}) => {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+    canvas.toBlob(onCapture);
+  };
+  return { videoRef, captureImage };
+};
+
+export default useCamera;
