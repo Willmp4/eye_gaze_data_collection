459869331f1cd906c6ccf73c0802354270cca48c
Willmp4 <54776440+Willmp4@users.noreply.github.com>
2023-12-30 16:37:41 +0000

    refactor so there is no eslint

--- frontend/src/CalibrationComponent.js
+++ frontend/src/CalibrationComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import "./CalibrationComponent.css";
 import useCamera from "./useCamera";
 import sendImageToServer from "./sendImageToServer";
@@ -9,47 +9,23 @@ function CalibrationComponent({ onCalibrationComplete, userId, setUserId }) {
   const { videoRef, captureImage } = useCamera();
   const [processing, setProcessing] = useState(null);
 
-  const screenData = {
-    screenWidth: window.screen.width,
-    screenHeight: window.screen.height,
-    devicePixelRatio: window.devicePixelRatio,
-  };
-
-  useEffect(() => {
-    const updatedCalibrationPoints = generateCalibrationPoints();
-    setCalibrationPoints(updatedCalibrationPoints);
-    // eslint-disable-next-line
-  }, []);
-  // eslint-disable-next-line
-  useEffect(() => {
-    const keyDownHandler = async (event) => {
-      if (event.keyCode === 32 && userId) {
-        await handleSpaceBar();
-      }
-    };
-
-    window.addEventListener("keydown", keyDownHandler);
-    return () => {
-      window.removeEventListener("keydown", keyDownHandler);
-    };
-    // Make sure to include all the dependencies required for the effect to work correctly
-    // eslint-disable-next-line
-  }, [currentPoint, calibrationPoints, userId]);
-  const generateCalibrationPoints = () => {
+  const generateCalibrationPoints = useCallback(() => {
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
     const offset = 20; // Offset from the edges
 
     const points = [
       { x: offset, y: offset }, // Adjusted from 0,0 to be within the screen
-      { x: offset, y: screenData.screenHeight - offset },
-      { x: screenData.screenWidth - offset, y: offset },
-      { x: screenData.screenWidth - offset, y: screenData.screenHeight - offset },
+      { x: offset, y: screenHeight - offset },
+      { x: screenWidth - offset, y: offset },
+      { x: screenWidth - offset, y: screenHeight - offset },
     ];
 
     const numCentralPoints = 46; // Number of points to be placed in the center
     const centralPointsPerSide = Math.ceil(Math.sqrt(numCentralPoints));
 
-    const stepX = (screenData.screenWidth - 2 * offset) / (centralPointsPerSide + 1);
-    const stepY = (screenData.screenHeight - 2 * offset) / (centralPointsPerSide + 1);
+    const stepX = (screenWidth - 2 * offset) / (centralPointsPerSide + 1);
+    const stepY = (screenHeight - 2 * offset) / (centralPointsPerSide + 1);
 
     for (let i = 1; i <= centralPointsPerSide; i++) {
       for (let j = 1; j <= centralPointsPerSide; j++) {
@@ -60,14 +36,24 @@ function CalibrationComponent({ onCalibrationComplete, userId, setUserId }) {
     }
 
     return points;
-  };
+  }, []);
 
-  const handleSpaceBar = async () => {
+  useEffect(() => {
+    const updatedCalibrationPoints = generateCalibrationPoints();
+    setCalibrationPoints(updatedCalibrationPoints);
+  }, [generateCalibrationPoints]);
+
+  const handleSpaceBar = useCallback(async () => {
     if (currentPoint < calibrationPoints.length) {
       const point = calibrationPoints[currentPoint];
       console.log("Point: ", point);
       try {
         const blob = await captureImage();
+        const screenData = {
+          screenWidth: window.screen.width,
+          screenHeight: window.screen.height,
+          devicePixelRatio: window.devicePixelRatio,
+        };
         const additionalData = {
           calibrationPoints: [point.x, point.y],
           userId: userId,
@@ -88,12 +74,33 @@ function CalibrationComponent({ onCalibrationComplete, userId, setUserId }) {
         console.error("Error capturing or sending image:", error);
       }
     }
-  };
+  }, [currentPoint, calibrationPoints, userId, captureImage, onCalibrationComplete]);
+
+  useEffect(() => {
+    const keyDownHandler = async (event) => {
+      if (event.keyCode === 32 && userId) {
+        await handleSpaceBar();
+      }
+    };
+
+    window.addEventListener("keydown", keyDownHandler);
+    return () => {
+      window.removeEventListener("keydown", keyDownHandler);
+    };
+    // Add dependencies to the dependency array
+  }, [userId, handleSpaceBar]);
 
   return (
     <div className={`calibration-container ${processing}`}>
       <video ref={videoRef} className="video-feed" />
-      <input type="text" placeholder="Enter User ID" value={userId} onChange={(e) => setUserId(e.target.value)} className="user-id-input" />
+      <input
+        type="text"
+        placeholder="Enter User ID"
+        value={userId}
+        onChange={(e) => setUserId(e.target.value)}
+        className="user-id-input"
+        aria-label="User ID"
+      />
       {calibrationPoints.length > 0 && (
         <div
           className="calibration-point"
@@ -101,6 +108,7 @@ function CalibrationComponent({ onCalibrationComplete, userId, setUserId }) {
             top: calibrationPoints[currentPoint].y,
             left: calibrationPoints[currentPoint].x,
           }}
+          aria-hidden="true"
         ></div>
       )}
     </div>
