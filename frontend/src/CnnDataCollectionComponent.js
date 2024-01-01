670e18f915acd39e33d17de8df06d479db5455c7
willmp4 <w.goudie@outlook.com>
2024-01-01 11:21:44 +0000

    Removed debounce

--- frontend/src/CnnDataCollectionComponent.js
+++ frontend/src/CnnDataCollectionComponent.js
@@ -2,8 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import "./CameraComponent.css";
 import useCamera from "./useCamera"; // Assuming you have this hook
 import sendImageToServer from "./sendImageToServer";
-// import debounce from "lodash.debounce";
-import { debounce } from "lodash";
+
 import getCameraParameters from "./getCameraParameters";
 function CameraComponent({ userId }) {
   const { videoRef, captureImage } = useCamera(); // Using the custom hook
@@ -38,9 +37,9 @@ function CameraComponent({ userId }) {
       }
     };
 
-    const updateCursorPosition = debounce((event) => {
+    const updateCursorPosition = (event) => {
       currentCursorPosition.current = { x: event.screenX, y: event.screenY };
-    }, 250);
+    };
 
     window.addEventListener("keydown", handleKeyDown);
     document.addEventListener("mousemove", updateCursorPosition);
@@ -48,7 +47,6 @@ function CameraComponent({ userId }) {
     return () => {
       document.removeEventListener("mousemove", updateCursorPosition);
       window.removeEventListener("keydown", handleKeyDown);
-      updateCursorPosition.cancel(); // Cancel the debounced call if component unmounts
     };
   }, [userId, captureImage, videoRef]);
 
