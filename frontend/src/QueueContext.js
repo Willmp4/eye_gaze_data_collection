459b5c9b163c4bd709214a6cbc74de21020377d5
willmp4 <w.goudie@outlook.com>
2024-01-04 12:33:50 +0000

    Add isProcessing state to QueueProvider

--- frontend/src/QueueContext.js
+++ frontend/src/QueueContext.js
@@ -7,6 +7,7 @@ export const useQueue = () => useContext(QueueContext);
 
 export const QueueProvider = ({ children }) => {
   const [taskQueue, setTaskQueue] = useState([]);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const addToQueue = (item) => {
     setTaskQueue((prev) => [...prev, item]);
@@ -14,19 +15,21 @@ export const QueueProvider = ({ children }) => {
 
   const processQueueItem = async (item) => {
     try {
+      setIsProcessing(true);
       const endpoint =
         item.type === "calibration"
           ? "https://gaze-detection-c70f9bc17dbb.herokuapp.com/calibrate"
           : "https://gaze-detection-c70f9bc17dbb.herokuapp.com/process-image";
 
       await sendImageToServer([item.data], endpoint);
+      setIsProcessing(false);
     } catch (error) {
       console.error("Error processing queue item:", error);
     }
   };
 
   useEffect(() => {
-    if (taskQueue.length > 0) {
+    if (taskQueue.length > 0 && !isProcessing) {
       const item = taskQueue[0];
       processQueueItem(item).then(() => {
         setTaskQueue((prev) => prev.slice(1));
