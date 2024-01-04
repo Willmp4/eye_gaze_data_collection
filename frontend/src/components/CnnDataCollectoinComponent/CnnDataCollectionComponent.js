57f9a763f3be25dfea6025a971e082c5b817e47a
willmp4 <w.goudie@outlook.com>
2024-01-04 10:52:30 +0000

    refactored

--- frontend/src/components/CnnDataCollectoinComponent/CnnDataCollectionComponent.js
+++ frontend/src/components/CnnDataCollectoinComponent/CnnDataCollectionComponent.js
@@ -1,86 +1,61 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./CnnDataCollectionComponent.css";
 import useCamera from "../../hooks/useCamera";
-import sendImageToServer from "../../utils/sendImageToServer";
+import useCacheManager from "../../hooks/useCacheManager";
+import useEventListeners from "../../hooks/useEventListeners";
 import getCameraParameters from "../../utils/getCameraParameters";
-import useCache from "../../utils/useCache";
 
 function CnnDataCollectionComponent({ userId }) {
   const { videoRef, captureImage } = useCamera();
+  const { cache, addToCache, processCache } = useCacheManager();
   const currentCursorPosition = useRef({ x: 0, y: 0 });
   const [isCapturing, setIsCapturing] = useState(false);
-  const [isProcessingQueue, setIsProcessingQueue] = useState(false);
-  const { cache, addToCache, removeFromCache } = useCache();
+  const [isProcessingCache, setIsProcessingCache] = useState(false);
 
-  const updateCursorPosition = useCallback((event) => {
+  const updateCursorPosition = (event) => {
     currentCursorPosition.current = { x: event.clientX, y: event.clientY };
-  }, []);
+  };
 
-  useEffect(() => {
-    window.addEventListener("mousemove", updateCursorPosition);
-    return () => {
-      window.removeEventListener("mousemove", updateCursorPosition);
-    };
-  }, [updateCursorPosition]);
-
-  const captureImageRequest = useCallback(async () => {
-    if (isCapturing || !userId) return;
+  const enqueueCapture = () => {
+    console.log("enqueueCapture");
+    if (!userId || isCapturing) return;
 
     setIsCapturing(true);
-    try {
-      const blob = await captureImage();
-      const { cameraMatrix, distCoeffs } = getCameraParameters(videoRef.current);
-      const currentCursorPos = currentCursorPosition.current;
 
-      const imageData = {
-        userId,
-        cursorPosition: JSON.stringify(currentCursorPos),
-        cameraMatrix: JSON.stringify(cameraMatrix),
-        distCoeffs: JSON.stringify(distCoeffs),
-        blob,
-      };
+    captureImage()
+      .then((blob) => {
+        const { cameraMatrix, distCoeffs } = getCameraParameters(videoRef.current);
 
-      addToCache(imageData);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsCapturing(false);
+        addToCache({
+          userId,
+          cursorPosition: JSON.stringify(currentCursorPosition.current),
+          cameraMatrix: JSON.stringify(cameraMatrix),
+          distCoeffs: JSON.stringify(distCoeffs),
+          blob,
+        });
+
+        setIsCapturing(false);
+      })
+      .catch((error) => {
+        console.error("Error capturing image:", error);
+        setIsCapturing(false);
+      });
+  };
+
+  useEventListeners("mousemove", updateCursorPosition);
+  useEventListeners("keydown", (event) => {
+    if (event.keyCode === 32) {
+      enqueueCapture();
     }
-  }, [isCapturing, userId, captureImage, videoRef, addToCache]);
-
-  const processQueue = useCallback(async () => {
-    if (isProcessingQueue || cache.length === 0) return;
-    setIsProcessingQueue(true);
-
-    // Clone the current cache for processing
-    const currentCache = [...cache];
-
-    for (let i = 0; i < currentCache.length; i++) {
-      try {
-        await sendImageToServer([currentCache[i]], "https://gaze-detection-c70f9bc17dbb.herokuapp.com/process-image");
-        removeFromCache(currentCache[i]);
-      } catch (error) {
-        console.error("Error processing image:", error);
-      }
+  });
+  useEffect(() => {
+    if (cache.length > 0 && !isProcessingCache) {
+      setIsProcessingCache(true);
+      processCache("https://gaze-detection-c70f9bc17dbb.herokuapp.com/process-image").finally(() => {
+        setIsProcessingCache(false);
+      });
     }
-
-    setIsProcessingQueue(false);
-  }, [cache, isProcessingQueue, removeFromCache]);
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.keyCode === 32) {
-        captureImageRequest();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [captureImageRequest]);
-
-  useEffect(() => {
-    processQueue();
-  }, [processQueue, cache]);
+  }, [cache, processCache, isProcessingCache]);
 
   return (
     <div className={`camera-container ${isCapturing ? "processing" : ""}`}>
