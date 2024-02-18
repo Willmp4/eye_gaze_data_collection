ae9272194187c74a3d8ab3a3c988426e341c3459
willmp4 <w.goudie@outlook.com>
2024-02-18 22:19:33 +0000

    Update file paths and remove unused code

--- data_processing/cropping_eyes_main.py
+++ data_processing/cropping_eyes_main.py
@@ -27,7 +27,7 @@ def main():
     print(f"Processed {len(X)} items.")
 
     # Save the processed data
-    with open('./pickel_files/all_data_200_100.pkl', 'wb') as f:
+    with open('./pickel_files/all_data_200_100_head_pose.pkl', 'wb') as f:
         pickle.dump((X,Y), f)
 
 
@@ -75,24 +75,20 @@ def get_combined_eyes(frame, global_detector, global_predictor, target_size=(200
             combined_eye_final_resized = combined_eye_final_resized.astype(np.float32) / 255.0
 
             return combined_eye_final_resized
-
     return None
 
 def compute_global_stats(base_dir):
     all_head_pose_data = []
     for csv_file in glob.glob(os.path.join(base_dir, '**', '*.csv'), recursive=True):
-        if 'calibration' in csv_file:
-            data = pd.read_csv(csv_file, usecols=[15, 16])
-            for _, row in data.iterrows():
-                head_pose_data = [float(x) for x in row.iloc[0].strip('"').split(',')]
-                head_translation_data = [float(x) for x in row.iloc[1].strip('"').split(',')]
-                all_head_pose_data.append(head_pose_data + head_translation_data)
+        data = pd.read_csv(csv_file, usecols=[15, 16])
+        for _, row in data.iterrows():
+            head_pose_data = [float(x) for x in row.iloc[0].strip('"').split(',')]
+            head_translation_data = [float(x) for x in row.iloc[1].strip('"').split(',')]
+            all_head_pose_data.append(head_pose_data + head_translation_data)
     all_head_pose_data = np.array(all_head_pose_data)
-    mean = np.mean(all_head_pose_data, axis=0)
-    std = np.std(all_head_pose_data, axis=0)
     min_vals = np.min(all_head_pose_data, axis=0)
     max_vals = np.max(all_head_pose_data, axis=0)
-    return mean, std, min_vals, max_vals
+    return  min_vals, max_vals
 
 # Assuming normalize_head_pose and get_combined_eyes are defined as before
 def get_screen_size(metadata_file_path):
@@ -124,7 +120,6 @@ def process_row(data, metadata_file_path, local_base_dir, min_vals, max_vals):
     head_translation = data[16]
     # Unpack the row data
     # image_path, cursor_x, cursor_y, *eye_box_pupil_data, head_pose, head_translation = data
-            # Check if the image exists in either directory
     eye_gaze_image_path = os.path.join(image_path)
     calibration_image_path = os.path.join(image_path)
 
@@ -142,21 +137,18 @@ def process_row(data, metadata_file_path, local_base_dir, min_vals, max_vals):
         print(f"Image not found: {full_image_path}")
         return None
     
-
     combined_eyes = get_combined_eyes(image, global_detector, global_predictor)
     if combined_eyes is None:
         return None
     
     # Normalize eye box pupil data
-    normalized_eye_box_pupil_data = [float(coord) / screen_width if i % 2 == 0 else float(coord) / screen_height for i, coord in enumerate(eye_box_pupil_data)]
+    # normalized_eye_box_pupil_data = [float(coord) / screen_width if i % 2 == 0 else float(coord) / screen_height for i, coord in enumerate(eye_box_pupil_data)]
 
     head_pose_data = [float(x) for x in head_pose.strip('"').split(',')]
     head_translation_data = [float(x) for x in head_translation.strip('"').split(',')]
     
     head_pose_data = np.array(head_pose_data + head_translation_data)
     normalized_head_pose_data = (head_pose_data - min_vals) / (max_vals - min_vals)
-
-    print(normalized_head_pose_data)
     
     # Normalize cursor positions
     normalized_cursor_x = float(cursor_x) / screen_width
@@ -167,26 +159,26 @@ def process_row(data, metadata_file_path, local_base_dir, min_vals, max_vals):
     # Y: Cursor position, eye box pupil data, head pose data
     X = combined_eyes
 # Assuming normalized_eye_box_pupil_data and normalized_head_pose_data are numpy arrays
-    Y = [normalized_cursor_x, normalized_cursor_y, full_image_path] + normalized_eye_box_pupil_data + normalized_head_pose_data.tolist()
+    Y = [normalized_cursor_x, normalized_cursor_y, full_image_path] +  normalized_head_pose_data.tolist()
     return X, Y
 
 def process_images_parallel(base_dir):
     subdirs = glob.glob(os.path.join(base_dir, '*/'))
-    mean, std, min_vals, max_vals = compute_global_stats(base_dir)
+    min_vals, max_vals = compute_global_stats(base_dir)
     
-    with Pool(processes=1) as pool:
+    with Pool() as pool:
         results = []
         for subdir in subdirs:
             print(f"Processing images in {subdir}")
             metadata_file_path = os.path.join(subdir, 'metadata.json')
             csv_files = glob.glob(os.path.join(subdir, '*.csv'))  # Correctly use glob.glob() here as well
 
-            csv_files = [csv_file for csv_file in csv_files if 'calibration' in csv_file]
+            # csv_files = [csv_file for csv_file in csv_files if 'calibration' in csv_file]
             
             for csv_file in csv_files:
                 print(f"Processing file: {csv_file}")
                 dataset = pd.read_csv(csv_file, header=None)
-                data_rows = [([row.values], metadata_file_path, base_dir, mean, std) for index, row in dataset.iterrows()]
+                data_rows = [([row.values], metadata_file_path, base_dir, min_vals, max_vals) for index, row in dataset.iterrows()]
                                 
                 processed_data = pool.starmap(process_row, [(row, metadata_file_path, base_dir, min_vals, max_vals) for row in data_rows])
                 results.extend(processed_data)
@@ -195,7 +187,6 @@ def process_images_parallel(base_dir):
     Y = [result[1] for result in results if result is not None]
 
     return X, Y
-
 
 if __name__ == '__main__':
     main()
